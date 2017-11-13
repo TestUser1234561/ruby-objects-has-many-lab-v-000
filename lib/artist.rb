4f21e9f67dd8c766bf52e9014159a2e022244f66
Morgan Fudge <mfudge92@gmail.com>
2017-11-13 18:18:28 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
 
   attr_accessor :name, :songs
-  @@songs = 0
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -11,18 +11,18 @@ class Artist
   def add_song(song)
     song.artist = self
     @songs << song
-    @@songs += 1
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     song.artist = self
     @songs << song
-    @@songs += 1
+    @@song_count += 1
   end
 
   def self.song_count
-    @@songs
+    @@song_count
   end
 
 end
